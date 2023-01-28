1b58191038794643145e6db9efa36443b97e12ae
Mehul Patel <mehul.citrusbug@gmail.com>
2023-01-28 17:31:08 +0530

    fetching path from getStaticpaths

--- pages/post/[postId].js
+++ pages/post/[postId].js
@@ -5,18 +5,18 @@ function PostDetails({ post }) {
 export default PostDetails;
 
 export async function getStaticPaths() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const posts = await response.json();
+
+  const paths = posts.map((data) => {
+    return {
+      params: {
+        postId: `${data.id}`,
+      },
+    };
+  });
   return {
-    paths: [
-      {
-        params: { postId: '1' },
-      },
-      {
-        params: { postId: '2' },
-      },
-      {
-        params: { postId: '3' },
-      },
-    ],
+    paths,
     fallback: false,
   };
 }
--- pages/post/index.js
+++ pages/post/index.js
@@ -27,7 +27,7 @@ export async function getStaticProps() {
 
   return {
     props: {
-      posts: posts.slice(0, 3),
+      posts: posts,
     },
   };
 }
